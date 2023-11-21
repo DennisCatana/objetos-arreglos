cd754056e8316977dccc9dc4ca30828b787665da
Washington <117743091+washito0407@users.noreply.github.com>
2023-11-20 20:03:08 -0800

    Update Arreglos.js

--- Arreglos.js
+++ Arreglos.js
@@ -1,24 +1,74 @@
-//hola
+//fundamentos:
+
+// Declaración de un array con algunos nombres
+let nombres = ['Juan', 'María', 'Pedro', 'Ana'];
+
+// Mostrar la cantidad de elementos en el array
+console.log('Cantidad de nombres:', nombres.length);
+
+// Mostrar todos los nombres y sus posiciones usando un bucle for
+console.log('Nombres y posiciones:');
+for (let i = 0; i < nombres.length; i++) {
+  console.log(`Posición ${i}: ${nombres[i]}`);
+}
+
+// Modificar un nombre en una posición específica
+nombres[1] = 'Luisa';
+console.log('Nombres después de la modificación:', nombres);
+
+// Agregar un nuevo nombre al final del array
+nombres.push('Carlos');
+console.log('Nombres después de agregar un nuevo nombre:', nombres);
+
+// Eliminar el primer nombre del array
+nombres.shift();
+console.log('Nombres después de eliminar el primer nombre:', nombres);
+
+// Recorrer el array utilizando forEach para mostrar cada nombre
+console.log('Mostrando nombres usando forEach:');
+nombres.forEach(function(nombre) {
+  console.log(nombre);
+});
 
 
+//Recorrido:
+
+let frutas = ['manzana', 'manzana', 'banana', 'uva', 'naranja'];
+
+console.log('Recorriendo dentro de un array de frutas:');
+
+frutas.forEach(function(fruta, indice) {
+  console.log(`Índice ${indice}: ${fruta}`);
+});
 
 
+//Metodos:
 
-//METODOS
-'use strict';
+let numeros = [2, 4, 6, 8, 10];
 
-const users = [
-  { id: 1, name: 'John' },
-  { id: 2, name: 'Jane' },
-  { id: 3, name: 'Bob' },
-  { id: 4, name: 'Alice' }
-];
+// Usando el método map() para duplicar cada número
+let numerosDuplicados = numeros.map(function(numero) {
+  return numero * 2;
+});
 
-// Buscar un usuario por ID
-const userId = 3;
-const foundUser = users.find(user => user.id === userId);
+console.log('Números originales:', numeros);
+console.log('Números duplicados:', numerosDuplicados);
 
-console.log(foundUser);
+// Usando el método filter() para obtener solo los números mayores que 5
+let mayoresQueCinco = numeros.filter(function(numero) {
+  return numero > 5;
+});
+
+console.log('Números mayores que 5:', mayoresQueCinco);
+
+// Usando el método reduce() para sumar todos los números del array
+let suma = numeros.reduce(function(acumulador, numero) {
+  return acumulador + numero;
+}, 0);
+
+console.log('Suma de todos los números:', suma);
+
+
 
 //DESTRUCTURACION
 function obtenerInformacion() {
