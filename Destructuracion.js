39aa1ea5d33fb0946e2f6dfe5c4d30b8b1b92d6b
Nayeli Barrera <117743538+NayeliBarrera@users.noreply.github.com>
2023-11-20 22:34:56 -0500

    Destructuracion

---
+++ Destructuracion.js
@@ -0,0 +1,9 @@
+function obtenerInformacion() {
+    return ['Brandon', 'Mena', 20];
+  }
+  
+  let [nombre, apellido, edad] = obtenerInformacion();
+  
+  console.log(nombre); 
+  console.log(apellido); 
+  console.log(edad); 
